560e18e04ae953f48c5e4632a31517a631401461
Bryan Octavio <100432689@alumnos.uc3m.es>
2023-03-27 13:33:53 +0200

    feat: modal view ui

--- app/index.tsx
+++ app/index.tsx
@@ -5,24 +5,29 @@ import {
   ScrollView,
   TouchableOpacity,
   Image,
+  Modal,
+  TextInput,
+  Button,
 } from "react-native";
 import { Stack } from "expo-router";
 
-import { COLORS, icons, SHADOWS, SIZES } from "../constants";
+import { COLORS, FONT, icons, SHADOWS, SIZES } from "../constants";
 import parcelsList from "../data/parcels_mm.json";
 import ParcelSlot from "../components/ParcelSlot";
-import { Parcel } from "../types";
 import { getUniqueParcelList } from "../utils/formatDate";
+import { useState } from "react";
 export default function Home() {
-
-const uniqueParcelLists=getUniqueParcelList(parcelsList)
-console.log(uniqueParcelLists);
+  const [modalVisible, setModalVisible] = useState(false);
+  const showModal = () => setModalVisible(true);
+  const hideModal = () => setModalVisible(false);
+  const uniqueParcelLists = getUniqueParcelList(parcelsList);
+  console.log(uniqueParcelLists);
   return (
     <SafeAreaView
       style={{
         backgroundColor: COLORS.lightWhite,
         paddingHorizontal: SIZES.large,
-        paddingTop:SIZES.medium,
+        paddingTop: SIZES.medium,
         flex: 1,
       }}
     >
@@ -31,7 +36,10 @@ console.log(uniqueParcelLists);
           headerStyle: {
             backgroundColor: COLORS.lightWhite,
           },
-          headerTitleStyle: { fontSize: SIZES.xLarge+4 },
+          headerTitleStyle: {
+            fontSize: SIZES.xLarge + 4,
+            fontFamily: FONT.medium,
+          },
           headerTitle: "Parcel Lists",
           headerShadowVisible: false,
         }}
@@ -39,8 +47,13 @@ console.log(uniqueParcelLists);
 
       <ScrollView showsVerticalScrollIndicator={false}>
         <View>
-          {uniqueParcelLists.map((parcel,index) => (<ParcelSlot key={parcel.parcel.id.$oid} parcelList={parcel} isLast={index === uniqueParcelLists.length-1 } />
-))}
+          {uniqueParcelLists.map((parcel, index) => (
+            <ParcelSlot
+              key={parcel.parcel.id.$oid}
+              parcelList={parcel}
+              isLast={index === uniqueParcelLists.length - 1}
+            />
+          ))}
         </View>
       </ScrollView>
       <View
@@ -48,7 +61,7 @@ console.log(uniqueParcelLists);
           alignItems: "center",
           width: "100%",
           backgroundColor: COLORS.lightWhite,
-          paddingVertical: 10,
+          paddingVertical: 15,
         }}
       >
         <TouchableOpacity
@@ -63,6 +76,7 @@ console.log(uniqueParcelLists);
             borderRadius: 50,
             ...SHADOWS.medium,
           }}
+          onPress={showModal}
         >
           <Image
             source={icons.addIcon}
@@ -71,6 +85,52 @@ console.log(uniqueParcelLists);
           />
         </TouchableOpacity>
       </View>
+      <Modal
+        visible={modalVisible}
+        transparent
+        animationType="slide"
+        onRequestClose={hideModal}
+      >
+        <SafeAreaView
+          style={{
+            backgroundColor: "#000000aa",
+            flex: 1,
+            flexDirection: "column-reverse",
+          }}
+        >
+          <View
+            style={{
+              height: "40%",
+              backgroundColor: COLORS.lightWhite,
+              opacity: 1,
+              padding: SIZES.medium,
+              borderTopLeftRadius: SIZES.medium,
+              borderTopRightRadius: SIZES.medium,
+              
+            }}
+          >
+            <Text style={{ fontSize: SIZES.xLarge, fontFamily: FONT.medium,textAlign:'center' }}>
+              Parcel and Carrier Information
+            </Text>
+
+            <View>
+              <TextInput
+                placeholder="Parcel ID"
+                style={{ backgroundColor: COLORS.lightWhite,fontSize:SIZES.large }}
+              ></TextInput>
+            </View>
+            <View>
+            <TextInput placeholder="Parcel Carrier"></TextInput>
+            </View>
+            <TouchableOpacity
+              style={{  backgroundColor: COLORS.tertiary }}
+              onPress={hideModal}
+            >
+              <Text style={{ textAlign: "center" }}>Save</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </Modal>
     </SafeAreaView>
   );
 }
