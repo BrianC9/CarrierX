05cadc28beff5cc832c16981bfd393bfe77711ab
Bryan Octavio <100432689@alumnos.uc3m.es>
2023-03-28 10:15:56 +0200

    feat: driver information form ui

--- app/parcel-details/[oid].tsx
+++ app/parcel-details/[oid].tsx
@@ -1,22 +1,56 @@
 import { useSearchParams, useRouter, Stack } from "expo-router";
-import { View, Text, SafeAreaView, FlatList } from "react-native";
+import { useCallback, useMemo, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+  TextInput,
+} from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
 import DeliveryParcel from "../../components/DeliveryParcel";
+import ModalForm from "../../components/ModalForm";
 import ParcelDetails from "../../components/ParcelDetails";
 import ParcelSlot from "../../components/ParcelSlot";
-import { COLORS, FONT, SIZES } from "../../constants";
+import { COLORS, FONT, SIZES, SHADOWS } from "../../constants";
 import parcelLists from "../../data/parcels_mm.json";
 import { Item, Parcel } from "../../types";
 import { formatDate, getListOfItems } from "../../utils/formatDate";
 
 function ParcelList() {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [succseModalVisible,setSuccessModalVisible] = useState(false)
+  const showModal = () => {
+    setModalVisible(true);
+  };
+  const hideModal = () => setModalVisible(false);
   const params = useSearchParams();
   const item = parcelLists.find(
     (parcelList: Parcel) => parcelList.id.$oid === params.oid
   );
-  const listOfItems = getListOfItems(item?.items ?? [])
-
+  const listOfItems = getListOfItems(item?.items ?? []);
+  const [driverData,setDriverData] = useState({
+    nameDriver:'',
+    plate:''
+  })
+  const handleDataFromTheDriver =()=>{
+    if(driverData.nameDriver.trim()==='' || driverData.nameDriver.trim()==='' ){
+      return 
+    }
+    alert(driverData.nameDriver+'\n'+driverData.plate)
+  }
   return (
-    <SafeAreaView style={{backgroundColor:COLORS.lightWhite,flex:1,padding:SIZES.medium}}>
+    <SafeAreaView
+      style={{
+        backgroundColor: COLORS.lightWhite,
+        flex: 1,
+        justifyContent: "space-between",
+        padding: SIZES.medium,
+      }}
+    >
       <Stack.Screen
         options={{
           title: `${params.oid?.toString().toLocaleUpperCase()} Parcel List`,
@@ -27,16 +61,173 @@ function ParcelList() {
           },
           headerStyle: {
             backgroundColor: COLORS.lightWhite,
-          
           },
-          headerTitleAlign:'left'
-
+          headerTitleAlign: "left",
         }}
       />
-      <Text>{params.nItems} to be picked up</Text>
-      {listOfItems.map((item,index) => <ParcelDetails key={item.id.$oid} isLast={index === listOfItems.length -1} item={item}/>)}
+      <View>
+        {listOfItems.map((item, index) => (
+          <ParcelDetails
+            key={item.id.$oid}
+            isLast={index === listOfItems.length - 1}
+            item={item}
+          />
+        ))}
+      </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: COLORS.red,
+          padding: SIZES.medium,
+          ...SHADOWS.medium,
+        }}
+        onPress={() => {}}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: SIZES.large,
+            color: COLORS.lightWhite,
+          }}
+          onPress={showModal}
+        >
+          DELIVERY
+        </Text>
+      </TouchableOpacity>
+      <Modal
+        visible={modalVisible}
+        transparent
+        animationType="slide"
+        onRequestClose={hideModal}
+      >
+        <SafeAreaView
+          style={{
+            backgroundColor: "#000000aa",
+            flex: 1,
+            flexDirection: "column-reverse",
+          }}
+        >
+          <View
+            style={{
+              height: "40%",
+              backgroundColor: COLORS.lightWhite,
+              opacity: 1,
+              padding: SIZES.medium,
+              borderTopLeftRadius: SIZES.medium,
+              borderTopRightRadius: SIZES.medium,
+              gap: 10,
+              justifyContent: "space-between",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: SIZES.xLarge,
+                fontFamily: FONT.medium,
+                textAlign: "center",
+              }}
+            >
+              Delivery Information
+            </Text>
+            <View style={{ gap: SIZES.medium }}>
+              <View
+                style={{
+                  borderWidth: 2,
+                  borderColor: COLORS.gray2,
+                  borderRadius: SIZES.xSmall / 2,
+                }}
+              >
+                <Text
+                  style={{
+                    position: "absolute",
+                    bottom: 45,
+                    left: 10,
+                    backgroundColor: COLORS.lightWhite,
+                    paddingHorizontal: SIZES.xSmall,
+                    color: COLORS.gray,
+                    zIndex: 100,
+                    fontSize: SIZES.small,
+                  }}
+                >
+                  Driver's Name
+                </Text>
+                <TextInput
+                  placeholder="Manfred Stager"
+                  value={driverData.nameDriver}
+                  onChangeText={(text) => {setDriverData(prevState =>({...prevState,nameDriver:text}))}}
+                  style={{
+                    backgroundColor: COLORS.lightWhite,
+                    fontSize: SIZES.large,
+                    padding: SIZES.small + 2,
+                  }}
+                />
+              </View>
+              <View
+               style={{
+                borderWidth: 2,
+                borderColor: COLORS.gray2,
+                borderRadius: SIZES.xSmall / 2,
+              }}
+              
+              >
+              <Text
+                style={{
+                  position: "absolute",
+                  bottom: 50,
+                  left: 10,
+                  backgroundColor: COLORS.lightWhite,
+                  paddingHorizontal: SIZES.xSmall,
+                  color: COLORS.gray,
+                  fontSize: SIZES.small,
+                  zIndex: 100,
+                }}
+              >
+                License Plate
+              </Text>
+              <TextInput
+                placeholder="3859FYF"
+                value={driverData.plate}
+                onChangeText={(text) => {setDriverData(prevState =>({...prevState,plate:text}))}}
+                style={{
+                  backgroundColor: COLORS.lightWhite,
+                  fontSize: SIZES.large,
+                  padding: SIZES.small + 2,
+                }}
+              ></TextInput>
+              </View>
+              <TouchableOpacity
+                style={{
+                  backgroundColor: COLORS.red,
+                  padding: SIZES.medium,
+                  ...SHADOWS.medium,
+                }}
+                onPress={handleDataFromTheDriver}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: SIZES.large,
+                    color: COLORS.lightWhite,
+                  }}
+                >
+                  NEXT
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </SafeAreaView>
+      </Modal>
     </SafeAreaView>
   );
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
 export default ParcelList;
